68979fdb8188ddba0f478b7a638235d04954f940
Ezzat Chamudi <echamudi@outlook.com>
2021-08-14 22:35:03 +0700

    Fix navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,25 +1,24 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
+import { useLocale, _ } from '../lib/i18n';
 import LocaleSwitcher from './LocaleSwitcher';
 
 const Navigation: React.FC = () => {
   const router = useRouter();
+  const l = useLocale();
   const isPosts = router.pathname.split('/')[2] === '[slug]';
 
   return (
     <nav className="flex flex-1 justify-center items-center">
-      <div className="font-bold mr-10">
-        Xerpihan
-      </div>
       <Link href="/">
-        <a className="text-gray-900 dark:text-white pr-6 py-4">Home</a>
-      </Link>
-      <Link href="/about">
-        <a className="text-gray-900 dark:text-white px-6 py-4">About</a>
+        <a className="text-gray-900 dark:text-white pr-6 py-4 font-bold ">Xerpihan</a>
       </Link>
       <Link href="/blog">
         <a className="text-gray-900 dark:text-white px-6 py-4">Blog</a>
+      </Link>
+      <Link href="/about">
+        <a className="text-gray-900 dark:text-white px-6 py-4">{_(l, 'Tentang', 'About')}</a>
       </Link>
       <div className="flex-1"></div>
 
