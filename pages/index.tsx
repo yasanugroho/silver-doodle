968ad01496c95774ef034f51e91e8501068fb07b
Ezzat Chamudi <echamudi@outlook.com>
2021-08-14 23:36:23 +0700

    Add estimation box

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import { useLocale, _ } from '../lib/i18n';
+import { L, useLocale, _ } from '../lib/i18n';
 
 // type IndexProps = {
 //   posts: PostType[];
@@ -11,19 +11,29 @@ export const Index: React.FC = () => {
 
   return (
     <Layout>
-      <h1>{_(l, 'Selamat Datang', 'Welcome')}</h1>
-      {_(l,
-        <p>Permulaian Next.js</p>,
-        <p>Next.js starter for your next blog or personal site. Built with:</p>
-      )}
+      <L>
+        <h1 className="text-6xl font-bold text-center">Layanan bahasa<br /><span className="x-text-primary">No.1</span> di Indonesia</h1>
+        <h1 className="text-6xl font-bold text-center">Indonesian <span className="x-text-primary">No.1</span><br /> language service provider</h1>
+      </L>
 
-      <ul className="list-disc pl-4 my-6">
-        <li>Next.js</li>
-        <li className="mt-2">Typescript</li>
-        <li className="mt-2">MDX</li>
-        <li className="mt-2">Tailwind CSS</li>
-      </ul>
+      <div className="flex justify-center flex-wrap mb-10">
+        <div className="mx-5 text-2xl font-bold text-gray-500">
+          Proofreading
+        </div>
+        <div className="mx-5 text-2xl font-bold text-gray-500">
+          <L>{'Terjemahan'}{'Translation'}</L>
+        </div>
+        <div className="mx-5 text-2xl font-bold text-gray-500">
+          <L>{'Subtitle'}{'Subtitles'}</L>
+        </div>
+        <div className="mx-5 text-2xl font-bold text-gray-500">
+          <L>{'Transkripsi'}{'Transcription'}</L>
+        </div>
+      </div>
 
+      <div className="border-2 rounded-md p-5">
+        Dapatkan estimasi biaya proofreading, terjemahan, subtitle, dan transkripsi, gratis!
+      </div>
     </Layout>
   );
 };
