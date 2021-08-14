81a2bd86821b3983effa5cd319611e2602f26d4f
Ezzat Chamudi <echamudi@outlook.com>
2021-08-14 22:19:58 +0700

    Change posts to blog

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -8,16 +8,25 @@ const Navigation: React.FC = () => {
   const isPosts = router.pathname.split('/')[2] === '[slug]';
 
   return (
-    <nav>
+    <nav className="flex flex-1 justify-center items-center">
+      <div className="font-bold mr-10">
+        Xerpihan
+      </div>
       <Link href="/">
         <a className="text-gray-900 dark:text-white pr-6 py-4">Home</a>
       </Link>
       <Link href="/about">
         <a className="text-gray-900 dark:text-white px-6 py-4">About</a>
       </Link>
+      <Link href="/blog">
+        <a className="text-gray-900 dark:text-white px-6 py-4">Blog</a>
+      </Link>
+      <div className="flex-1"></div>
 
       {/* Hide language switcher in posts */}
-      {!isPosts && <LocaleSwitcher />}
+      <div className="mr-5">
+        {!isPosts && <LocaleSwitcher />}
+      </div>
     </nav>
   );
 };
