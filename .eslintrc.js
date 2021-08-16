c4f4e2fb23328650947304c96a418626d6ce1964
Ezzat Chamudi <echamudi@outlook.com>
2021-08-16 20:21:55 +0700

    Remove max len

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     quotes: ['error', 'single'],
     'react-native/no-inline-styles': 0,
     'no-unused-vars': 'off',
-    'max-len': ['error', { code: 120 }],
     '@typescript-eslint/no-unused-vars': 0,
   },
 };
