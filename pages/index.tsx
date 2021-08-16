7660fc1e9ae82a86f5a199334f8378e5d1e95272
Ezzat Chamudi <echamudi@outlook.com>
2021-08-16 17:56:39 +0700

    Update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,10 +16,10 @@ export const Index: React.FC = () => {
           <h1 className="text-6xl font-bold text-center">Layanan bahasa<br /><span className="text-xerpihan-primary-600">No.1</span> di Indonesia</h1>
           <h1 className="text-6xl font-bold text-center">Indonesian <span className="text-xerpihan-primary-600">No.1</span><br /> language service provider</h1>
         </L>
-        <div className="flex justify-center flex-wrap mb-10">
+        <div className="flex justify-center flex-wrap mb-5">
           <div className="mx-5 text-2xl font-bold text-gray-500">
-            Proofreading
-        </div>
+            <L>{'Proofreading'}{'Proofreading'}</L>
+          </div>
           <div className="mx-5 text-2xl font-bold text-gray-500">
             <L>{'Terjemahan'}{'Translation'}</L>
           </div>
@@ -30,8 +30,10 @@ export const Index: React.FC = () => {
             <L>{'Transkripsi'}{'Transcription'}</L>
           </div>
         </div>
-        <div className="border-2 rounded-md p-5">
-          Dapatkan estimasi biaya proofreading, terjemahan, subtitle, dan transkripsi, gratis!
+        <div className="max-w-5xl px-8 py-4 mx-auto">
+          <div className="border-2 rounded-md p-5 bg-white">
+            Dapatkan estimasi biaya proofreading, terjemahan, subtitle, dan transkripsi, gratis!
+          </div>
         </div>
       </div>
       <Footer />
