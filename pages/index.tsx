52ef26b959e273222866f7f67824f09a3fcaed81
Ezzat Chamudi <echamudi@outlook.com>
2021-08-16 19:28:59 +0700

    Update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export const Index: React.FC = () => {
           <h1 className="text-6xl font-bold text-center">Layanan bahasa<br /><span className="text-xerpihan-primary-600">No.1</span> di Indonesia</h1>
           <h1 className="text-6xl font-bold text-center">Indonesian <span className="text-xerpihan-primary-600">No.1</span><br /> language service provider</h1>
         </L>
-        <div className="flex justify-center flex-wrap mb-5">
+        <div className="flex justify-center flex-wrap pb-5">
           <div className="mx-5 text-2xl font-bold text-gray-500">
             <L>{'Proofreading'}{'Proofreading'}</L>
           </div>
@@ -30,8 +30,14 @@ export const Index: React.FC = () => {
             <L>{'Transkripsi'}{'Transcription'}</L>
           </div>
         </div>
-        <div className="max-w-5xl px-8 py-4 mx-auto">
-          <div className="border-2 rounded-md p-5 bg-white">
+      </div>
+      <div className="relative">
+        <div className="w-full h-full flex flex-col absolute">
+          <div className="flex-1 bg-xerpihan-hero-gradient-end dark:bg-gray-900"></div>
+          <div className="flex-1 "></div>
+        </div>
+        <div className="max-w-5xl px-8 py-4 mx-auto relative">
+          <div className="border-2 rounded-md p-5 bg-white dark:bg-black">
             Dapatkan estimasi biaya proofreading, terjemahan, subtitle, dan transkripsi, gratis!
           </div>
         </div>
