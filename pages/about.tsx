d6ea2c5bea67c5b59883df9a311e5b07ed13d690
aisyahmuhaddisi <aisyahmuhaddisi@mail.ugm.ac.id>
2021-08-30 01:18:04 +0700

    case study, about us, blog done

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,10 @@
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React from 'react';
+import Header from '../components/Header';
 import Layout from '../components/Layout';
+import LayoutPlain from '../components/LayoutPlain';
+import profile4 from '../images/Profile4.png';
 import { _ } from '../lib/i18n';
 
 export const Page: React.FC = () => {
@@ -8,13 +12,88 @@ export const Page: React.FC = () => {
   const { locale: l } = router;
 
   return (
-    <Layout
+    <LayoutPlain
       customMeta={{
         title: _(l, 'Tentang', 'About') + ' - Xerpihan',
       }}>
-      <h1>{_(l, 'Tentang', 'About')}</h1>
-      <p>Hello World</p>
-    </Layout>
+      <div
+        className="bg-gradient-to-b
+        from-xerpihan-hero-gradient-start to-xerpihan-hero-gradient-end dark:from-gray-900 dark:to-gray-800">
+        <Header></Header>
+        <div className="text-left max-w-5xl mx-auto py-10">
+          <h1 className="text-xerpihan-primary-500 text-5xl font-bold">Tentang Kami</h1>
+          <p className="text-2xl font-light">
+            Perusahaan startup teknologi yang membantu memperbaiki dokumen dalam Bahasa Inggris dan Bahasa Indonesia.
+            Sedang dalam pengembangan Al-based platform dalam menyediakan jasa proofread, translate, pembuatan subtitle
+            dan transkripsi berkualitas.
+          </p>
+        </div>
+      </div>
+      <div className="max-w-5xl mx-auto text-center py-10">
+        <h1 className="text-xerpihan-primary-500 text-4xl font-bold">Team Kami</h1>
+        <p className="text-2xl font-light">
+          Perusahaan startup teknologi yang membantu memperbaiki dokumen dalam Bahasa Inggris dan Bahasa Indonesia.
+          Sedang dalam pengembangan Al-based platform dalam menyediakan jasa proofread, translate, pembuatan subtitle
+          dan transkripsi berkualitas.
+        </p>
+      </div>
+      <div className="max-w-100 bg-xerpihan-hero-gradient-end">
+        <div className="mx-auto grid grid-cols-3 gap-10 max-w-5xl py-10">
+          <div className="relative text-center">
+            <Image src={profile4} alt="..." width="120" height="120" className="rounded-full object-cover" />
+            <div className="bg-white border border-gray-300 p-5 rounded-lg" style={{ marginTop: '-70px' }}>
+              <h1 className="text-base font-bold mb-0" style={{ marginTop: '70px' }}>
+                Albert Donko
+              </h1>
+              <h1 className="text-base font-light text-gray-500 mb-0">Universitas Gadjah Mada</h1>
+              <p className="text-sm font-light text-justify tracking-tight">
+                Terima kasih teman-teman, pertahankan kerja bagus! Xerpihan harus dinominasikan untuk service of the
+                year. Anda tidak akan menyesalinya.
+              </p>
+            </div>
+          </div>
+          <div className="relative text-center">
+            <Image src={profile4} alt="..." width="120" height="120" className="rounded-full object-cover" />
+            <div className="bg-white border border-gray-300 p-5 rounded-lg" style={{ marginTop: '-70px' }}>
+              <h1 className="text-base font-bold mb-0" style={{ marginTop: '70px' }}>
+                Albert Donko
+              </h1>
+              <h1 className="text-base font-light text-gray-500 mb-0">Universitas Gadjah Mada</h1>
+              <p className="text-sm font-light text-justify tracking-tight">
+                Terima kasih teman-teman, pertahankan kerja bagus! Xerpihan harus dinominasikan untuk service of the
+                year. Anda tidak akan menyesalinya.
+              </p>
+            </div>
+          </div>
+          <div className="relative text-center">
+            <Image src={profile4} alt="..." width="120" height="120" className="rounded-full object-cover" />
+            <div className="bg-white border border-gray-300 p-5 rounded-lg" style={{ marginTop: '-70px' }}>
+              <h1 className="text-base font-bold mb-0" style={{ marginTop: '70px' }}>
+                Albert Donko
+              </h1>
+              <h1 className="text-base font-light text-gray-500 mb-0">Universitas Gadjah Mada</h1>
+              <p className="text-sm font-light text-justify tracking-tight">
+                Terima kasih teman-teman, pertahankan kerja bagus! Xerpihan harus dinominasikan untuk service of the
+                year. Anda tidak akan menyesalinya.
+              </p>
+            </div>
+          </div>
+          <div className="relative text-center">
+            <Image src={profile4} alt="..." width="120" height="120" className="rounded-full object-cover" />
+            <div className="bg-white border border-gray-300 p-5 rounded-lg" style={{ marginTop: '-70px' }}>
+              <h1 className="text-base font-bold mb-0" style={{ marginTop: '70px' }}>
+                Albert Donko
+              </h1>
+              <h1 className="text-base font-light text-gray-500 mb-0">Universitas Gadjah Mada</h1>
+              <p className="text-sm font-light text-justify tracking-tight">
+                Terima kasih teman-teman, pertahankan kerja bagus! Xerpihan harus dinominasikan untuk service of the
+                year. Anda tidak akan menyesalinya.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </LayoutPlain>
   );
 };
 
--- pages/case-studies.tsx
+++ pages/case-studies.tsx
@@ -5,6 +5,12 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import LayoutPlain from '../components/LayoutPlain';
 import illustration from '../images/IllustrationCaseStudy.svg';
+import brandLogo from '../images/BrandLogo2.svg';
+import profile1 from '../images/Profile1.png';
+import profile2 from '../images/Profile2.png';
+import profile3 from '../images/Profile3.png';
+import profile4 from '../images/Profile4.png';
+import KoranTempo from '../images/KoranTempo.png';
 import { L, _ } from '../lib/i18n';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
@@ -39,7 +45,7 @@ export const Page: React.FC = () => {
       }}>
       <div
         className="bg-gradient-to-b
-        from-xerpihan-hero-gradient-start to-xerpihan-hero-gradient-end dark:from-gray-900 dark:to-gray-800 py-10">
+        from-xerpihan-hero-gradient-start to-xerpihan-hero-gradient-end dark:from-gray-900 dark:to-gray-800 pb-10">
         <Header></Header>
         <div className="mx-auto container px-8 max-w-5xl">
           <h1 className="text-5xl font-bold text-left">
@@ -65,20 +71,20 @@ export const Page: React.FC = () => {
           </p>
         </div>
       </div>
-      <div className="max-w-5xl mx-auto grid-cols-3 grid py-10">
+      <div className="max-w-5xl mx-auto p-4 md:px-0 md:py-10 md:grid-cols-3 md:grid">
         <div className="text-left">
           <h1 className="text-4xl text-xerpihan-primary-500 font-bold mb-0">Studi Kasus</h1>
           <p className="text-lg text-gray-500 font-light">
             Kami percaya merek dan perusahaan adalah penghubung antara perusahaan dengan pelanggan mereka.
           </p>
-          <Image src={illustration} alt=""></Image>
+          <Image className="hidden md:block" src={illustration} alt=""></Image>
         </div>
         <div className="col-span-2">
           <Carousel
             additionalTransfrom={0}
             arrows
             autoPlay
-            autoPlaySpeed={1000}
+            autoPlaySpeed={2000}
             centerMode={false}
             className=""
             containerClass="container-with-dots"
@@ -121,7 +127,7 @@ export const Page: React.FC = () => {
             sliderClass=""
             slidesToSlide={2}
             swipeable>
-            <div className="flex-row m-4">
+            <div className="flex flex-col m-4">
               <div className="bg-xerpihan-primary-500 text-white mt-4 text-left max-w-xl rounded-xl p-3">
                 <h1 className="text-2xl text-bold mb-0">Implementation of fuzzy logic</h1>
                 <p className="text-lg text-light">Institut Teknologi Surabaya</p>
@@ -145,7 +151,7 @@ export const Page: React.FC = () => {
                 </div>
               </div>
             </div>
-            <div className="flex-row m-4">
+            <div className="flex flex-col-reverse m-4">
               <div className="bg-xerpihan-primary-500 text-white mt-4 text-left max-w-xl rounded-xl p-3">
                 <h1 className="text-2xl text-bold mb-0">Implementation of fuzzy logic</h1>
                 <p className="text-lg text-light">Institut Teknologi Surabaya</p>
@@ -169,7 +175,7 @@ export const Page: React.FC = () => {
                 </div>
               </div>
             </div>
-            <div className="flex-row m-4">
+            <div className="flex-col flex m-4">
               <div className="bg-xerpihan-primary-500 text-white mt-4 text-left max-w-xl rounded-xl p-3">
                 <h1 className="text-2xl text-bold mb-0">Implementation of fuzzy logic</h1>
                 <p className="text-lg text-light">Institut Teknologi Surabaya</p>
@@ -194,6 +200,134 @@ export const Page: React.FC = () => {
               </div>
             </div>
           </Carousel>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 max-w-5xl mx-auto py-10">
+        <Image src={brandLogo} alt=""></Image>
+        <Image src={brandLogo} alt=""></Image>
+      </div>
+      <div className="max-w-3xl mx-auto text-center py-10">
+        <h1 className="pb-10 text-4xl text-xerpihan-primary-500 font-bold mb-0">Apa Kata Mereka?</h1>
+        <div className="grid grid-cols-2">
+          <div className="h-full">
+            <Image src={profile1} alt=""></Image>
+          </div>
+          <div className="text-left p-3">
+            <h1 className="text-xl font-light text-black">
+              Terima kasih teman-teman, pertahankan kerja bagus! Xerpihan harus dinominasikan untuk service of the year.
+              Anda tidak akan menyesalinya.
+            </h1>
+            <p className="text-md font-bold mb-0">Alberto Donko</p>
+            <p className="text-md font-light text-gray-500">CEO, Squareround</p>
+          </div>
+        </div>
+        <div className="grid grid-cols-2">
+          <div className="text-left p-3">
+            <h1 className="text-xl font-light text-black">
+              Menghemat waktu dan tenaga saya, persis seperti yang kurang dari bisnis kami. Xerpihan sangat berharga
+              bagi perusahaan saya.
+            </h1>
+            <p className="text-md font-bold mb-0">Alex Parkinson</p>
+            <p className="text-md font-light text-gray-500">Director, MyTravel.com</p>
+          </div>
+          <div className="h-full">
+            <Image src={profile2} alt=""></Image>
+          </div>
+        </div>
+        <div className="grid grid-cols-2">
+          <div className="h-full">
+            <Image src={profile3} alt=""></Image>
+          </div>
+          <div className="text-left p-3">
+            <h1 className="text-xl font-light text-black">
+              Xerpihan adalah sumber daya bisnis paling berharga yang pernah kami beli.
+            </h1>
+            <p className="text-md font-bold mb-0">Elizabeth Martin</p>
+            <p className="text-md font-light text-gray-500">CEO, Abcs</p>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-100 bg-gray-700 text-center py-10">
+        <h1 className="text-white text-4xl font-bold">Komentar Akademisi</h1>
+        <div className="max-w-5xl grid grid-cols-4 gap-4 mx-auto">
+          <div className="p-5 rounded-lg relative bg-white">
+            <Image src={profile4} alt="..." width="120" height="120" className="rounded-full object-cover" />
+            <h1 className="text-base font-bold mb-0 mt-3">Albert Donko</h1>
+            <h1 className="text-base font-light text-gray-500 mb-0">Universitas Gadjah Mada</h1>
+            <p className="text-sm font-light text-justify tracking-tight">
+              Terima kasih teman-teman, pertahankan kerja bagus! Xerpihan harus dinominasikan untuk service of the year.
+              Anda tidak akan menyesalinya.
+            </p>
+          </div>
+          <div className="p-5 rounded-lg relative bg-white">
+            <Image src={profile4} alt="..." width="120" height="120" className="rounded-full object-cover" />
+            <h1 className="text-base font-bold mb-0 mt-3">Albert Donko</h1>
+            <h1 className="text-base font-light text-gray-500 mb-0">Universitas Gadjah Mada</h1>
+            <p className="text-sm font-light text-justify tracking-tight">
+              Terima kasih teman-teman, pertahankan kerja bagus! Xerpihan harus dinominasikan untuk service of the year.
+              Anda tidak akan menyesalinya.
+            </p>
+          </div>
+          <div className="p-5 rounded-lg relative bg-white">
+            <Image src={profile4} alt="..." width="120" height="120" className="rounded-full object-cover" />
+            <h1 className="text-base font-bold mb-0 mt-3">Albert Donko</h1>
+            <h1 className="text-base font-light text-gray-500 mb-0">Universitas Gadjah Mada</h1>
+            <p className="text-sm font-light text-justify tracking-tight">
+              Terima kasih teman-teman, pertahankan kerja bagus! Xerpihan harus dinominasikan untuk service of the year.
+              Anda tidak akan menyesalinya.
+            </p>
+          </div>
+          <div className="p-5 rounded-lg relative bg-white">
+            <Image src={profile4} alt="..." width="120" height="120" className="rounded-full object-cover" />
+            <h1 className="text-base font-bold mb-0 mt-3">Albert Donko</h1>
+            <h1 className="text-base font-light text-gray-500 mb-0">Universitas Gadjah Mada</h1>
+            <p className="text-sm font-light text-justify tracking-tight">
+              Terima kasih teman-teman, pertahankan kerja bagus! Xerpihan harus dinominasikan untuk service of the year.
+              Anda tidak akan menyesalinya.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="text-center py-10 max-w-5xl mx-auto">
+        <h1 className="text-xerpihan-primary-500 text-4xl font-bold">Diliput Berbagai Media</h1>
+        <div className="grid grid-cols-4 py-4">
+          <Image src={KoranTempo} alt=""></Image>
+          <div className="col-span-3 text-left">
+            <p className="text-base font-light">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pretium aliquet mauris, vitae consequat
+              ante dictum sed. Cras ultricies consectetur dui vitae elementum. Quisque in ante dignissim, facilisis diam
+              ac, commodo dolor. Nulla vel felis ut eros aliquam dictum sed vel metus. Donec ac vulputate sapien, a
+              rutrum justo. Etiam fringilla ante non felis pellentesque, id mollis mauris viverra.
+            </p>
+            <h1 className="text-base font-bold mb-0 mt-3">Elizabeth Martin</h1>
+            <h1 className="text-base font-light text-gray-500 mb-0">Reporter Koran Tempo</h1>
+          </div>
+        </div>
+        <div className="grid grid-cols-4 py-4">
+          <Image src={KoranTempo} alt=""></Image>
+          <div className="col-span-3 text-left">
+            <p className="text-base font-light">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pretium aliquet mauris, vitae consequat
+              ante dictum sed. Cras ultricies consectetur dui vitae elementum. Quisque in ante dignissim, facilisis diam
+              ac, commodo dolor. Nulla vel felis ut eros aliquam dictum sed vel metus. Donec ac vulputate sapien, a
+              rutrum justo. Etiam fringilla ante non felis pellentesque, id mollis mauris viverra.
+            </p>
+            <h1 className="text-base font-bold mb-0 mt-3">Elizabeth Martin</h1>
+            <h1 className="text-base font-light text-gray-500 mb-0">Reporter Koran Tempo</h1>
+          </div>
+        </div>
+        <div className="grid grid-cols-4 py-4">
+          <Image src={KoranTempo} alt=""></Image>
+          <div className="col-span-3 text-left">
+            <p className="text-base font-light">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pretium aliquet mauris, vitae consequat
+              ante dictum sed. Cras ultricies consectetur dui vitae elementum. Quisque in ante dignissim, facilisis diam
+              ac, commodo dolor. Nulla vel felis ut eros aliquam dictum sed vel metus. Donec ac vulputate sapien, a
+              rutrum justo. Etiam fringilla ante non felis pellentesque, id mollis mauris viverra.
+            </p>
+            <h1 className="text-base font-bold mb-0 mt-3">Elizabeth Martin</h1>
+            <h1 className="text-base font-light text-gray-500 mb-0">Reporter Koran Tempo</h1>
+          </div>
         </div>
       </div>
       <Footer></Footer>
