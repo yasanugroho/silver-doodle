5597350fb59fad5853b861fa843707364dd42f02
Ezzat Chamudi <echamudi@outlook.com>
2021-09-17 14:16:04 +0700

    Disable eslint errors

--- next.config.js
+++ next.config.js
@@ -25,6 +25,11 @@ const config = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   // @ts-ignore
   images: {
     domains: ['http://localhost:3000'],
