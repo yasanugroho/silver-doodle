43f5f33b2b651bf16e8e55404cb240a847880508
devxerpihan <77371906+devxerpihan@users.noreply.github.com>
2021-09-20 12:08:54 +0700

    Update IndividualsVariables.tsx

--- utils/IndividualsVariables.tsx
+++ utils/IndividualsVariables.tsx
@@ -4,11 +4,11 @@ export const listLayanan = [
   {
     img: ServicesIcon1,
     titleId: 'Lebih dari 1.000 Klien Mempercayai Xerpihan',
-    titleEn: 'Over Than 1.000 Clients Trust Xerpihan',
+    titleEn: 'More Than 1.000 Clients Trust Xerpihan',
     subTitleId:
       'Jasa proofreading, translate, subtitle dan transkripsi kami telah membantu banyak pihak mulai dari institusi pemerintah, perusahaan swasta, hingga YouTuber.',
     subTitleEn:
-      'Jasa proofreading, translate, subtitle dan transkripsi kami telah membantu banyak pihak mulai dari institusi pemerintah, perusahaan swasta, hingga YouTuber.',
+      'Our proofreading, translation, subtitling and transcription services have helped many government institutions, private companies, and YouTubers.',
     styling: '',
   },
   {
@@ -16,7 +16,7 @@ export const listLayanan = [
     titleId: 'Cukup Sekali Pesan',
     titleEn: 'Order Once',
     subTitleId: 'Kami siap memberikan garansi revisi tanpa batas untuk setiap pemesanan jasa proofreading, terjemah, subtitle, atau transkripsi.',
-    subTitleEn: 'Once you order, enjoy the service until you are satisfied. No revision limit.',
+    subTitleEn: 'We are ready to offer unlimited revision guarantee for every order of proofreading, translation, subtitle, and transcription service.',
     styling: '',
   },
   {
@@ -24,7 +24,7 @@ export const listLayanan = [
     titleId: 'Ahli Bahasa Profesional',
     titleEn: 'Profesional Translator',
     subTitleId: 'Proofreader dan penerjemah kami merupakan lulusan Sastra Inggris yang sudah berpengalaman menyunting dan menerjemahkan dokumen serta membuat subtitle dan transkrip.',
-    subTitleEn: 'English Major Graduates, Use Newest Reference',
+    subTitleEn: 'Our proofreaders and translators are English Literature graduates who are experienced in editing and translating documents and also in subtitling and transcribing',
     styling: '',
   },
   {
@@ -43,11 +43,11 @@ export interface serviceText {
 }
 
 export const text: serviceText[] = [
-  { id: 'Proofreading jurnal dan skripsi', en: 'Script and thesis proofreading' },
-  { id: 'Subtitle YouTube dan film', en: 'Document translate' },
-  { id: 'Terjemah jurnal dan artikel', en: 'Subtitle on youtube channel' },
+  { id: 'Proofreading jurnal dan skripsi', en: 'Journal and thesis proofreading' },
+  { id: 'Subtitle YouTube dan film', en: 'YouTube and film subtitle' },
+  { id: 'Terjemah jurnal dan artikel', en: 'Journal and article translation' },
   { id: 'Transkrip wawancara', en: 'Audio clip transcript' },
-  { id: 'Parafrasa kalimat', en: 'Proposal perbaikan' },
+  { id: 'Parafrasa kalimat', en: 'Sentences paraphrase' },
   { id: '…dan masih banyak lagi, sesuaikan dengan kebutuhan anda!', en: '..and many more, ' },
   { id: '', en: '' },
 ];
