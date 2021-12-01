1d0faa4289c85a67f888439c453b58035cc0d8e5
devxerpihan <77371906+devxerpihan@users.noreply.github.com>
2021-12-01 16:09:54 +0700

    update meta

--- components/Head.tsx
+++ components/Head.tsx
@@ -7,8 +7,8 @@ import { MetaProps } from '../types/layout';
 const Head: React.FC<{ customMeta?: MetaProps }> = ({ customMeta }) => {
   const router = useRouter();
   const meta: MetaProps = {
-    title: 'Xerpihan',
-    description: 'Your personal writing assistant',
+    title: 'Xerpihan | 1st Indonesian Translation and Proofreading Startup Company',
+    description: 'First AI Startup in Indonesia for English and Indonesian text correction. Hands on your documents and videos to us for best tech-enabled translation, proofreading, subtitle, and transcription service.',
     image: `${WEBSITE_HOST_URL}/images/site-preview.png`,
     type: 'website',
     ...customMeta,
