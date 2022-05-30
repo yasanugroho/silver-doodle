29cfb2fdf5bcb00feb2086aac952952828f194f2
Yasa <yasinagungnugroho@gmail.com>
2022-05-30 21:41:38 +0700

    Add files via upload

---
+++ types/post.ts
@@ -0,0 +1,9 @@
+export type PostType = {
+  date?: string;
+  description?: string;
+  image?: string;
+  otherLangs?: Record<string, string>;
+  slug: string;
+  title: string;
+  lang: string;
+};
